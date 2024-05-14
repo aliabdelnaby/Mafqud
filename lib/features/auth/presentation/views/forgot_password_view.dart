d4412b6462579ecfc9399722336c73f7bf51d722
aliabdelnaby <aliabdelnabi239@gmail.com>
2024-05-15 01:07:40 +0300

    Finish Forgot password UI

--- lib/features/auth/presentation/views/forgot_password_view.dart
+++ lib/features/auth/presentation/views/forgot_password_view.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:mafqud/features/auth/presentation/widgets/custom_reset_password_form.dart';
+import 'package:mafqud/features/auth/presentation/widgets/custom_title_login_view.dart';
 
 class ForgotPasswordView extends StatelessWidget {
   const ForgotPasswordView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: SizedBox(height: MediaQuery.sizeOf(context).height * 0.1),
+            ),
+            const SliverToBoxAdapter(child: CustomTitleLoginView()),
+            SliverToBoxAdapter(
+              child: SizedBox(height: MediaQuery.sizeOf(context).height * 0.1),
+            ),
+            const SliverToBoxAdapter(child: CustomResetPasswordForm()),
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/features/auth/presentation/widgets/custom_reset_password_form.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:mafqud/core/utils/app_colors.dart';
+import 'package:mafqud/core/widgets/custom_btn.dart';
+import 'package:mafqud/features/auth/presentation/widgets/custom_text_field.dart';
+
+class CustomResetPasswordForm extends StatefulWidget {
+  const CustomResetPasswordForm({super.key});
+
+  @override
+  State<CustomResetPasswordForm> createState() =>
+      _CustomResetPasswordFormState();
+}
+
+class _CustomResetPasswordFormState extends State<CustomResetPasswordForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      child: Column(
+        children: [
+          CustomTextFormField(
+            hintText: "Email",
+            onChanged: (email) {},
+          ),
+          SizedBox(height: MediaQuery.of(context).size.height * 0.03),
+          CustomBtn(
+            backgroundColor: AppColors.primary,
+            onPressed: () async {
+              if (_formKey.currentState!.validate()) {}
+            },
+            text: "Send link to reset password",
+            height: MediaQuery.of(context).size.height * 0.05,
+            width: double.infinity,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
