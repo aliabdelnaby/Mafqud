ce7f80199ba0943564325917b6ef01056256a4ba
aliabdelnaby <aliabdelnabi239@gmail.com>
2024-05-21 22:28:01 +0300

    Add my posts UI [1]

--- lib/features/my_posts/views/my_post_view.dart
+++ lib/features/my_posts/views/my_post_view.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:mafqud/features/my_posts/widgets/custom_my_mafqud_card.dart';
 
 class MyPostsView extends StatelessWidget {
   const MyPostsView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('MyPosts Screen'),
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 5),
+        child: ListView.builder(
+          itemCount: 5,
+          itemBuilder: (context, index) {
+            return const CustomMyMafqudCard();
+          },
+        ),
+      ),
     );
   }
 }
